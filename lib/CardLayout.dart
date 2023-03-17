dd46b4bc49411b8d10b535cb883a38aaf74a3a95
Palla.Bhargav <palla.bhargav@magicbricks.com>
2023-03-17 14:29:01 +0530

    flutter learning

--- lib/CardLayout.dart
+++ lib/CardLayout.dart
@@ -15,7 +15,7 @@ class OrderCard extends StatelessWidget {
     return Container(
 
         child:Card(
-          margin: EdgeInsets.all(16),
+          margin: const EdgeInsets.all(16),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8),
           ),
@@ -62,13 +62,14 @@ class OrderCard extends StatelessWidget {
               ),
               TagsWidget(),
               Padding(
-                  padding: EdgeInsets.fromLTRB(16,0,0,16),
+                  padding: const EdgeInsets.fromLTRB(16,0,0,16),
                   child: Column(
                       children: _orderDetails())
               ),
               _separator(),
               quoteWidget("quote"),
-              quoteWidget("timer")
+              quoteWidget("timer"),
+              acceptDeclineButtons()
 
             ],
           ),
@@ -92,7 +93,7 @@ List<Widget> _orderDetails() {
 
 Widget orderItem(OrderInfo item) {
   return Container(
-    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+    margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
     child: Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.start,
@@ -101,13 +102,13 @@ Widget orderItem(OrderInfo item) {
         Expanded(
           flex: 3,
           //widthFactor: 0.5,
-          child: Text(item.label, style: TextStyle(fontSize: 15, color: Colors.black),),
+          child: Text(item.label, style: const TextStyle(fontSize: 15, color: Colors.black),),
         ),
-        Text(":"),
+        const Text(":"),
         Expanded(
-          flex: 6,
-            child: Padding(padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
-            child:Text(item.value,  style: TextStyle(fontSize: 15, color: Colors.black),))
+            flex: 6,
+            child: Padding(padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                child:Text(item.value,  style: const TextStyle(fontSize: 15, color: Colors.black),))
         )
 
       ],
@@ -121,7 +122,7 @@ Widget _separator() {
     builder: (BuildContext context, BoxConstraints constraints) {
       final boxWidth = constraints.constrainWidth();
       const dashWidth = 8.0;
-      final dashHeight = 1.0;
+      const dashHeight = 1.0;
       final dashCount = (boxWidth / (2 * dashWidth)).floor();
       return Flex(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -161,11 +162,58 @@ Widget quoteWidget(String type) {
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         svg,
-         Padding(padding: const EdgeInsets.fromLTRB(12, 0, 0, 0),
-        child: Text(text),
+        Padding(padding: const EdgeInsets.fromLTRB(12, 0, 0, 0),
+          child: Text(text),
         )
       ],
     ),
   );
+
+}
+
+Widget acceptDeclineButtons() {
+  return Container(
+
+    margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Expanded(child:
+          ElevatedButton(
+            onPressed: () { print("hello");
+            },
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.white),
+                padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(32, 8, 32, 8)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        side: const BorderSide(color: Colors.blue, width: 1, strokeAlign: BorderSide.strokeAlignInside)
+                    )
+                )
+            ),
+            child: const Text(
+              'Cancel',textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 14), textScaleFactor: 1,
+            ),
+          )),
+          const SizedBox(width: 16,),
+          Expanded(child:
+          ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(32, 8, 32, 8)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )
+                  )
+              ),
+              child: const Text("Confirm", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 14),)
+
+          )
+          )
+        ],
+      ));
 
 }
